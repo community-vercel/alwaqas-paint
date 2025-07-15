4b71315ab96a989ca8196392f3a6791f46e4413a
Anisul Islam <anis.inbox10@gmail.com>
2025-07-15 22:49:46 +0500

    fix  website redirects for indexing

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'alwaqaspaint.com' }],
+        destination: 'https://www.alwaqaspaint.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
